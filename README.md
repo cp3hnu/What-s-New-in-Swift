b000b4c3951a4ae428fb481540148cff7ceef994
zhaowei <>
2019-10-14 16:01:08 +0800

    资料整理

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# What-s-New-in-Swift
-What's New in Swift学习资料和笔记
+# What's New in Swift
+From Swift 4.0 to Swift 5.1
+
+-   [Swift 4.0](https://github.com/ole/whats-new-in-swift-4)
+-   [Swift 4.1](https://github.com/twostraws/whats-new-in-swift-4-1)
+-   [Swift 4.2](https://github.com/twostraws/whats-new-in-swift-4-2)
+-   [Swift 5.0](https://github.com/twostraws/whats-new-in-swift-5-0)
+-   [Swift 5.1](https://github.com/twostraws/whats-new-in-swift-5-1)
+
+
+
+#### Website
+
+[What's New in Swift by Paul Hudson](https://www.whatsnewinswift.com/?from=5.0&to=5.1)
---
+++ Whats-New-In-Swift-5-1.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+/*:
+ # What’s new in Swift 5.1
+
+ * Created by [Paul Hudson](https://twitter.com/twostraws) – [Hacking with Swift](https://www.hackingwithswift.com)
+ * Last update: May 10th 2019
+
+ This playground is designed to showcase new features introduced with Swift 5.1. I already [wrote an article on it](https://www.hackingwithswift.com/articles/182/whats-new-in-swift-5-1), but it's a lot more fun to see things in action and experiment yourself.
+
+ If you hit problems or have questions, you're welcome to tweet me [@twostraws](https://twitter.com/twostraws) or email <paul@hackingwithswift.com>.
+
+ - important: Until a version of Xcode ships with Swift 5.1, you should [download the latest Swift trunk development snapshot](https://swift.org/download/) and activate it inside your current Xcode version.
+
+ &nbsp;
+
+ ## Contents
+
+ * [Improvements to synthesized memberwise initializers](Improvements%20to%20synthesized%20memberwise%20initializers)
+ * [Implicit returns from single-expression functions](Implicit%20returns%20from%20single-expression%20functions)
+ * [Universal Self](Universal%20Self)
+ * [Opaque return types](Opaque%20return%20types)
+ * [Static and class subscripts](Static%20and%20class%20subscripts)
+ * [Warnings for ambiguous none cases](Warnings%20for%20ambiguous%20none%20cases)
+ * [Matching optional enums against non-optionals](Matching%20optional%20enums%20against%20non-optionals)
+ * [Ordered collection diffing](Ordered%20collection%20diffing)
+ * [Creating uninitialized arrays](Creating%20uninitialized%20arrays)
+
+ &nbsp;
+ 
+ [Next >](@next)
+ */
